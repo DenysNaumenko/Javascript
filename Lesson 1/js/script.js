018dc052baedf1cf80a64708c478275ded29dc45
Denys <denysnaumenko1@gmail.com>
2023-09-16 21:21:32 +0300

    first commit

---
+++ Lesson 1/js/script.js
@@ -0,0 +1,145 @@
+
+// // "use strict";
+// // a = 15;
+// // console.log(a);
+
+
+// // let number = 5;
+// // const leftBorderWidth = 1;
+
+// // number = 10;
+// // console.log(number);
+
+// // const obj = {
+// //     a:50
+// // };
+
+// // obj.a = 10;
+
+// // console.log(obj);
+
+// // console.log(name);
+// // var name = 'Ivan';
+
+// // {
+// //     var result = 50;
+// // }
+
+// // console.log(result);
+
+
+// // ['fgdf', 'fgfgfgh']
+
+// // snake_case
+// // UPPER_SNAKE_CASE
+// // Kebab-case
+// // PascalCase
+
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string'*9);
+
+// const persone = "Alex";
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+             
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp'];
+// console.log(arr[1]);
+
+// const arr = ['a','b','c'];
+// arr[10]= '3456';
+// console.log(arr[10]);
+
+
+
+// const arrObj = {
+//     0:'a',
+//     1:'b',
+//     2:'c',
+//     abc:{
+//         df:[{}, {} ],
+//         def:{
+
+//         }
+//     }
+// }
+// const b ='b';
+
+// //  arrObj.b ='1234'
+// arrObj['b'] = '1234';
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+
+
+// // const obj = { a:1, b: 2};
+
+// const obj = {
+//     'Anna':500,
+//     'Alice':800
+// };
+
+
+// alert ('Helo');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers =[];
+// answers[0]= prompt('Как ваше имя?', '');
+// answers[1]= prompt('Как ваша фамилия?', '');
+// answers[2]= prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет,${user}`);
+
+операторы 15
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+
+let incr = 10,
+    decr = 10;
+
+
+    // incr++;
+    // decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+
+    console.log(5%2);
+
+    console.log(2 + 2 *2 !== '6' );
+
+    const isChecked = true,
+           isClose = false;
+
+           console.log(isChecked || !isClose);
---
+++ index.js
@@ -0,0 +1,9 @@
+const arr = [5];
+function f() {
+
+}
+
+//npm Init
+//npm init @eslint/config
+
+ 
